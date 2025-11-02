77198a855ff953472fc6ce29482253b3d7381070
Gunjesh Nayak <gunjeshnayak12@gmail.com>
2025-11-02 17:45:51 +0530

    requirements_added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name='HealthAIChatBot',
+    version='1.0.0',
+    author='Gunjesh Nayak', 
+    author_email='abc.onlinehost@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
+
+
+
